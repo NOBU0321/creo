a2d158fee632b5a97b9166fbf0b13fc1fdf23c48
NOBU0321 <60180339+NOBU0321@users.noreply.github.com>
2020-04-17 00:00:04 +0900

    aaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_083251) do
+ActiveRecord::Schema.define(version: 2020_04_16_115930) do
 
   create_table "pins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "description"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2020_04_13_083251) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "bio"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
